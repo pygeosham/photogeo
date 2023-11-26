4e946570b3e3b0fa17ede037d1109d88a7d3e4d3
pygeosham <pygeosham@gmail.com>
2023-11-26 22:55:56 +0900

    webmap本体と設定ファイルを追加
    webmap本体とspredsheetおよびformsを読み込むための設定を記述するファイルを追加

---
+++ constants.js
@@ -0,0 +1,8 @@
+//Sheets API KEY
+const API_KEY = 'APIKEYを記述してください';
+//formsの出力先スプレッドシートID
+const SHEET_ID = 'スプレッドシートIDを記述してください';
+//シート名の入力
+const SHEET_NAME = 'シート名を記述してください';
+//formsのURL
+const FORMS_URL = 'FORMSのURLを記述してください';
